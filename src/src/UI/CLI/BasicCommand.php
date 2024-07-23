98243ecca95ac926961588d7101e14ef3d4035b4
Gemma <gemma.cortel@gmail.com>
2024-07-23 15:35:35 +0200

    Updating to Symfony 7

--- src/src/UI/CLI/BasicCommand.php
+++ src/src/UI/CLI/BasicCommand.php
@@ -2,19 +2,15 @@
 
 namespace App\UI\CLI;
 
-use App\Application\SuspiciousReadingsFinder;
-use App\Infrastructure\Repository\ClientRepositoryResolver;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'app:basic-command')]
 class BasicCommand extends Command
 {
-    protected static $defaultName = 'app:basic-command';
-
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->writeln('This is a basic command');
 
